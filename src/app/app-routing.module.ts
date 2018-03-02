2780a8bbcdd619c59deff169a9b6b33eeed30715
Rod Pattison <rodpattison@gmail.com>
2018-03-02 11:09:23 +0800

    Added home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DecoderComponent } from './decoder/decoder.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path : 'decoder', component: DecoderComponent}];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'decoder', component: DecoderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { DecoderComponent } from './decoder/decoder.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DecoderComponent
+    DecoderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
