5eb46f67b480c39ed0f9d7bace340415d2ec374a
Rod Pattison <rodpattison@gmail.com>
2018-03-03 09:43:09 +0800

    Added form input and model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
@@ -18,6 +19,7 @@ import { DecoderComponent } from './decoder/decoder.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule.forRoot(),
     AppRoutingModule
   ],
--- src/app/decoder/decoder.component.ts
+++ src/app/decoder/decoder.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
-  selector: 'app-decoder',
-  templateUrl: './decoder.component.html',
-  styleUrls: ['./decoder.component.scss']
+  selector    : 'app-decoder',
+  templateUrl : './decoder.component.html',
+  styleUrls   : [ './decoder.component.scss' ]
 })
 export class DecoderComponent implements OnInit {
 
-  constructor() { }
+  canFrame: string                = '';
+  canFrameChange: Subject<string> = new Subject<string>();
+
+  pgns = {
+    61442 : {
+      name    : 'Electronic Transmission Controller 1',
+      acronym : 'ETC1',
+      spns    : []
+    }
+  };
+
+  constructor() {
+    this.canFrameChange
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .subscribe(decode);
+  }
 
   ngOnInit() {
   }
+
+  onChange(text: string) {
+    console.log('changing');
+    this.canFrameChange.next(text);
+  }
+
+  decode(canFrame: string) {
+    console.log('changed');
+    this.canFrame = canFrame;
+
+  }
+
 
 }
