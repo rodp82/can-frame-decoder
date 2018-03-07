5e47534d866d01eaf6fd2bb04129f6c716fa55a5
Rod Pattison <rodpattison@gmail.com>
2018-03-08 06:21:49 +0800

    Fixed onChange event

--- src/app/decoder/decoder.component.ts
+++ src/app/decoder/decoder.component.ts
@@ -3,45 +3,41 @@ import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 
+
 @Component({
-  selector    : 'app-decoder',
+  selector : 'app-decoder',
   templateUrl : './decoder.component.html',
-  styleUrls   : [ './decoder.component.scss' ]
+  styleUrls : [ './decoder.component.scss' ]
 })
 export class DecoderComponent implements OnInit {
 
-  canFrame: string                = '';
+  canFrame: string = '0CF00401#FFFF82DF1AFFFFFF';
   canFrameChange: Subject<string> = new Subject<string>();
 
   pgns = {
     61442 : {
-      name    : 'Electronic Transmission Controller 1',
+      name : 'Electronic Transmission Controller 1',
       acronym : 'ETC1',
-      spns    : []
+      spns : []
     }
   };
 
   constructor() {
-    this.canFrame = '0CF00401#FFFF82DF1AFFFFFF';
     this.canFrameChange
       .debounceTime(500)
       .distinctUntilChanged()
-      .subscribe(this.decode);
+      .subscribe(newStr => this.decode(newStr));
   }
 
   ngOnInit() {
   }
 
   onChange(text: string) {
-    console.log('changing');
     this.canFrameChange.next(text);
   }
 
   decode(canFrame: string) {
-    console.log('changed');
     this.canFrame = canFrame;
-
   }
-
 
 }
