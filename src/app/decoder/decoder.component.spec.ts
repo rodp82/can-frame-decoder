04b41477e5a73ea7f3241ed64816dc034e24850a
Rod Pattison <rodpattison@gmail.com>
2018-03-21 13:31:36 +0800

    Fixed errors in test files

--- src/app/decoder/decoder.component.spec.ts
+++ src/app/decoder/decoder.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DecoderComponent } from './decoder.component';
+import { FormsModule } from '@angular/forms';
+import { DecoderService } from '../services/decoder.service';
 
 describe('DecoderComponent', () => {
   let component: DecoderComponent;
@@ -8,13 +10,15 @@ describe('DecoderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DecoderComponent ]
+      declarations : [ DecoderComponent ],
+      imports      : [ FormsModule ],
+      providers    : [ DecoderService ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DecoderComponent);
+    fixture   = TestBed.createComponent(DecoderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
